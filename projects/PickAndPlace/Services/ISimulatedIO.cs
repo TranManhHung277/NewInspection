d80be8b1fc305bbd8bc9fa3b753a6adaeea84750
Nguyen Van Nguyen <ngvan.nguyen164@gmail.com>
2026-01-23 22:12:41 +0700

    update

--- projects/PickAndPlace/Services/ISimulatedIO.cs
+++
@@ -1,6 +0,0 @@
-namespace PickAndPlace.Services;
-
-public interface ISimulatedIO
-{
-    void SetInput(string address, bool value);
-}
--- projects/PickAndPlace/Services/NullSimulatedIO.cs
+++
@@ -1,9 +0,0 @@
-namespace PickAndPlace.Services;
-
-public sealed class NullSimulatedIO : ISimulatedIO
-{
-    public void SetInput(string address, bool value)
-    {
-        // No-op for real hardware mode.
-    }
-}
