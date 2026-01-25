90231b6137239d8f78c6543c520c156165973742
Nguyen Van Nguyen <ngvan.nguyen164@gmail.com>
2026-01-25 13:02:04 +0700

    up[

--- src/UI/NAutoSuite.UI.Controls/KeyboardInput.xaml.cs
+++ src/UI/NAutoSuite.UI.Controls/KeyboardInput.xaml.cs
@@ -1,5 +1,8 @@
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 using NAutoSuite.UI.Controls.Dialogs;
 
 namespace NAutoSuite.UI.Controls;
@@ -13,6 +16,18 @@ public partial class KeyboardInput : UserControl
     public static readonly DependencyProperty PendingTextProperty =
         DependencyProperty.Register(nameof(PendingText), typeof(string), typeof(KeyboardInput),
             new PropertyMetadata(string.Empty, OnPendingTextChanged));
+
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register(nameof(Value), typeof(object), typeof(KeyboardInput),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnValueChanged));
+
+    public static readonly DependencyProperty ValueTypeProperty =
+        DependencyProperty.Register(nameof(ValueType), typeof(KeyboardInputValueType), typeof(KeyboardInput),
+            new PropertyMetadata(KeyboardInputValueType.String));
+
+    public static readonly DependencyProperty DisplayDecimalsProperty =
+        DependencyProperty.Register(nameof(DisplayDecimals), typeof(int), typeof(KeyboardInput),
+            new PropertyMetadata(-1, OnDisplayDecimalsChanged));
 
     public static readonly DependencyProperty KeyboardTypeProperty =
         DependencyProperty.Register(nameof(KeyboardType), typeof(KeyboardInputType), typeof(KeyboardInput),
@@ -49,6 +64,7 @@ public partial class KeyboardInput : UserControl
     private bool _isDialogOpen;
     private bool _suppressNextFocus;
     private bool _isSyncing;
+    private bool _suppressValueFormat;
 
     public KeyboardInput()
     {
@@ -65,6 +81,24 @@ public partial class KeyboardInput : UserControl
     {
         get => (string)GetValue(PendingTextProperty);
         set => SetValue(PendingTextProperty, value);
+    }
+
+    public object? Value
+    {
+        get => GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    public KeyboardInputValueType ValueType
+    {
+        get => (KeyboardInputValueType)GetValue(ValueTypeProperty);
+        set => SetValue(ValueTypeProperty, value);
+    }
+
+    public int DisplayDecimals
+    {
+        get => (int)GetValue(DisplayDecimalsProperty);
+        set => SetValue(DisplayDecimalsProperty, value);
     }
 
     public KeyboardInputType KeyboardType
@@ -122,6 +156,11 @@ public partial class KeyboardInput : UserControl
             return;
         }
 
+        if (BindingOperations.IsDataBound(control, ValueProperty))
+        {
+            return;
+        }
+
         control._isSyncing = true;
         control.PendingText = e.NewValue?.ToString() ?? string.Empty;
         control._isSyncing = false;
@@ -134,12 +173,50 @@ public partial class KeyboardInput : UserControl
             return;
         }
 
-        if (!control.CommitOnEnter)
+        if (control.CommitOnEnter)
         {
-            control._isSyncing = true;
-            control.Text = e.NewValue?.ToString() ?? string.Empty;
-            control._isSyncing = false;
+            return;
         }
+
+        if (BindingOperations.IsDataBound(control, ValueProperty))
+        {
+            control.CommitPendingText(formatAfterCommit: false);
+            return;
+        }
+
+        control._isSyncing = true;
+        control.Text = e.NewValue?.ToString() ?? string.Empty;
+        control._isSyncing = false;
+    }
+
+    private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not KeyboardInput control || control._isSyncing)
+        {
+            return;
+        }
+
+        if (control._suppressValueFormat)
+        {
+            return;
+        }
+
+        control.SetPendingFromValue(e.NewValue);
+    }
+
+    private static void OnDisplayDecimalsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not KeyboardInput control)
+        {
+            return;
+        }
+
+        if (!BindingOperations.IsDataBound(control, ValueProperty))
+        {
+            return;
+        }
+
+        control.SetPendingFromValue(control.Value);
     }
 
     private static void OnCommitOnEnterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -186,7 +263,7 @@ public partial class KeyboardInput : UserControl
             return;
         }
 
-        Text = PendingText;
+        CommitPendingText(formatAfterCommit: true);
         e.Handled = true;
     }
 
@@ -208,17 +285,14 @@ public partial class KeyboardInput : UserControl
         {
             var owner = Window.GetWindow(this);
             string? result = KeyboardType == KeyboardInputType.Numeric
-                ? NumericKeyboardDialog.Show(owner, Text, AllowDecimal, AllowNegative)
-                : TextKeyboardDialog.Show(owner, Text);
+                ? NumericKeyboardDialog.Show(owner, PendingText, AllowDecimal, AllowNegative)
+                : TextKeyboardDialog.Show(owner, PendingText);
 
             if (result != null)
             {
                 PendingText = result;
 
-                if (CommitOnEnter)
-                {
-                    Text = result;
-                }
+                CommitPendingText(formatAfterCommit: true);
             }
         }
         finally
@@ -227,10 +301,139 @@ public partial class KeyboardInput : UserControl
             _suppressNextFocus = true;
         }
     }
+
+    private void CommitPendingText(bool formatAfterCommit)
+    {
+        if (!TryConvert(PendingText, out var converted))
+        {
+            if (BindingOperations.IsDataBound(this, ValueProperty))
+            {
+                SetPendingFromValue(Value);
+            }
+            return;
+        }
+
+        if (BindingOperations.IsDataBound(this, ValueProperty))
+        {
+            _suppressValueFormat = !formatAfterCommit;
+            Value = converted;
+            _suppressValueFormat = false;
+            return;
+        }
+
+        _isSyncing = true;
+        Text = converted?.ToString() ?? string.Empty;
+        _isSyncing = false;
+    }
+
+    private void SetPendingFromValue(object? value)
+    {
+        var formatted = FormatValue(value);
+
+        _isSyncing = true;
+        PendingText = formatted;
+        if (!BindingOperations.IsDataBound(this, TextProperty))
+        {
+            Text = formatted;
+        }
+        _isSyncing = false;
+    }
+
+    private string FormatValue(object? value)
+    {
+        if (value == null)
+        {
+            return string.Empty;
+        }
+
+        var culture = CultureInfo.CurrentCulture;
+        var decimals = DisplayDecimals;
+
+        return ValueType switch
+        {
+            KeyboardInputValueType.String => value.ToString() ?? string.Empty,
+            KeyboardInputValueType.Int32 => Convert.ToInt32(value, culture).ToString(culture),
+            KeyboardInputValueType.Float => FormatNumber(Convert.ToSingle(value, culture), decimals, culture),
+            KeyboardInputValueType.Double => FormatNumber(Convert.ToDouble(value, culture), decimals, culture),
+            KeyboardInputValueType.Decimal => FormatNumber(Convert.ToDecimal(value, culture), decimals, culture),
+            _ => value.ToString() ?? string.Empty
+        };
+    }
+
+    private static string FormatNumber<T>(T value, int decimals, CultureInfo culture) where T : struct, IFormattable
+    {
+        return decimals >= 0
+            ? value.ToString($"F{decimals}", culture)
+            : value.ToString(null, culture);
+    }
+
+    private bool TryConvert(string input, out object? converted)
+    {
+        converted = null;
+
+        if (ValueType == KeyboardInputValueType.String)
+        {
+            converted = input;
+            return true;
+        }
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return false;
+        }
+
+        var culture = CultureInfo.CurrentCulture;
+        switch (ValueType)
+        {
+            case KeyboardInputValueType.Int32:
+                if (int.TryParse(input, NumberStyles.Integer, culture, out var intValue))
+                {
+                    converted = intValue;
+                    return true;
+                }
+                return false;
+
+            case KeyboardInputValueType.Float:
+                if (float.TryParse(input, NumberStyles.Float, culture, out var floatValue))
+                {
+                    converted = floatValue;
+                    return true;
+                }
+                return false;
+
+            case KeyboardInputValueType.Double:
+                if (double.TryParse(input, NumberStyles.Float, culture, out var doubleValue))
+                {
+                    converted = doubleValue;
+                    return true;
+                }
+                return false;
+
+            case KeyboardInputValueType.Decimal:
+                if (decimal.TryParse(input, NumberStyles.Number, culture, out var decimalValue))
+                {
+                    converted = decimalValue;
+                    return true;
+                }
+                return false;
+
+            default:
+                return false;
+        }
+    }
 }
 
 public enum KeyboardInputType
 {
     Text,
     Numeric
+}
+
+public enum KeyboardInputValueType
+{
+    String,
+    Int32,
+    Float,
+    Double,
+    Decimal
 }
